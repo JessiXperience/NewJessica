071fca7fff289963aa7d5caa8ccd30650e3da2d7
CodedLife <dcodedlife@gmail.com>
2023-04-22 00:53:47 +0300

    Initial commit

---
+++ src/main/java/me/codedlife/newjessica/client/NewJessicaClient.java
@@ -0,0 +1,13 @@
+package me.codedlife.newjessica.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NewJessicaClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
